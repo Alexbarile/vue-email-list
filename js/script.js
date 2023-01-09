49376a430cbc2ea1262ea03f09ce2a6e98ed5b2c
“Alexbarile” <alexbarile93fg@yahoo.it>
2023-01-09 17:40:12 +0100

    BONUS, mostro a schermo le email

--- js/script.js
+++ js/script.js
@@ -9,14 +9,12 @@ createApp({
         }
     },
     mounted() {
+        // ciclo for per stampare 10 email
         for(let i=0; i<10; i++){
             axios.get('https://flynn.boolean.careers/exercises/api/random/mail').then((result)=> {
                 this.email = result.data.response;
-                console.log(result.data.response)
-                
+                this.emailArray.push(this.email);
             });
-
         }
-        
     },
 }).mount('#app')
